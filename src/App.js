78d8468847987c4a3c15e35d6dac603b31f8258c
David Moyer (713422) <DMoyer@smail.pembinatrails.ca>
2021-10-26 09:41:26 -0500

    Finished the backgrounds for sections 1-3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import './SideNav.js';
+import SideNav from './SideNav.js';
 
 function App() {
   return (
     <>
       <SideNav />
-      <section id="section-1"></section>
+      <section id="title-card">
+        <h1>Driving in the Dark</h1>
+        <h2>And the dangers and challenges it presents.</h2>
+      </section>
       <section id="section-2"></section>
       <section id="section-3"></section>
       <section id="section-4"></section>
--- src/SideNav.js
+++ src/SideNav.js
@@ -1,24 +1,36 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function SideNav() {
 
   let [open, setOpen] = useState(false);
 
+  let toggleNav = () => {
+    setOpen(!open);
+  }
+
   return (
-    <nav id="side-nav" className={"" + (open ? "open" : "")}>
-      <button id="toggle-nav">Close</button>
-      <ul>
-        <li>
-          <Link to="">Link 1</Link>
-        </li>
-        <li>
-          <Link to="">Link 2</Link>
-        </li>
-        <li>
-          <Link to="">Link 3</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav id="side-nav" className={"" + (open ? "open" : "")}>
+        <button id="toggle-nav" onClick={toggleNav}>
+          {open ? "ᐊ" : "ᐅ"}
+        </button>
+        <ul>
+          <li>
+            <a href="#title-card">Section 1</a>
+          </li>
+          <li>
+            <a href="#section-2">Section 2</a>
+          </li>
+          <li>
+            <a href="#section-3">Section 3</a>
+          </li>
+          <li>
+            <a href="#section-4">Section 4</a>
+          </li>
+        </ul>
+      </nav>
+      <div id="side-nav-overlay" className={open ? "visible" : ""}></div>
+    </>
   )
 }
 
